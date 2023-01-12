43fcc0d59879aa60b25d1897e42e83d03fafb000
pslivinskas <povilaslsivinskas@gmail.com>
2023-01-12 13:18:29 +0200

    Exercise dalyba with exceptions

---
+++ src/main/java/Dalyba/MainDalyba.java
@@ -0,0 +1,14 @@
+package Dalyba;
+
+public class MainDalyba {
+    public static void main(String[] args) {
+
+        String[] masyvas = InputManager.askForInput();
+
+        try {
+            System.out.println(IntDivider.divide(masyvas));
+        } catch (ArgumentIsZeroException e) {
+            System.out.println(e);
+        }
+    }
+}
